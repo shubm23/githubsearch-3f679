29fd19d0cc8ca4c63019f1d02fe5c4fc8d4eb560
shubm23 <shubm23@gmail.com>
2022-04-03 23:40:33 +0530

    First Changes

---
+++ src/pages/About.jsx
@@ -0,0 +1,21 @@
+function About() {
+  return (
+    <>
+      <h1 className='text-6xl mb-4'>Github Finder</h1>
+      <p className='mb-4 text-2xl font-light'>
+        A React app to search GitHub profiles and see profile details
+      </p>
+      <p className='text-lg text-gray-400'>
+        Designed By:
+        <a
+          className='text-white'
+          href='https://www.linkedin.com/in/shubham-mandal-67bb7b125/'
+        >
+          Shubham Mandal
+        </a>
+      </p>
+    </>
+  )
+}
+
+export default About
